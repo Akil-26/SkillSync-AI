a7a2e426fced3de2e88dc300dd0964a3c4656af9
Akil26 <akil20052622@gmail.com>
2026-07-03 09:41:42 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,35 +3,17 @@ import './Layout.css'
 import Sidebar from './Sidebar'
 import CandidatePanel from './CandidatePanel'
 
-function Layout({
-    jobDescription,
-    setJobDescription,
-    preferences,
-    setPreferences,
-    jdFiles,
-    setJdFiles,
-    resumeFiles,
-    setResumeFiles
-}) {
-    return (
-        <div className="layout-container">
-            <div className="layout-sidebar">
-                <Sidebar
-                    jobDescription={jobDescription}
-                    setJobDescription={setJobDescription}
-                    preferences={preferences}
-                    setPreferences={setPreferences}
-                    jdFiles={jdFiles}
-                    setJdFiles={setJdFiles}
-                    resumeFiles={resumeFiles}
-                    setResumeFiles={setResumeFiles}
-                />
-            </div>
-            <div className="layout-main">
-                <CandidatePanel resumeFiles={resumeFiles} />
-            </div>
-        </div>
-    )
+function Layout(props) {
+  return (
+    <div className="layout-container">
+      <div className="layout-sidebar">
+        <Sidebar {...props} />
+      </div>
+      <div className="layout-main">
+        <CandidatePanel {...props} />
+      </div>
+    </div>
+  )
 }
 
 export default Layout
